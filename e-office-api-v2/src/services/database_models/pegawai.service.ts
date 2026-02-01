028a3bb2e09169f7d6bf34fb3472cabf53ab5611
irvinokent <irvinokent14@gmail.com>
2026-02-02 00:16:56 +0700

    fix(master data): CRUD Role fungsional

--- e-office-api-v2/src/services/database_models/pegawai.service.ts
+++ e-office-api-v2/src/services/database_models/pegawai.service.ts
@@ -4,6 +4,10 @@ import { Prisma, type Pegawai } from "@backend/db/index.ts";
 import type {
 	PegawaiDelegate,
 	PegawaiInclude,
+	PegawaiCreateInput,
+	PegawaiUpdateInput,
+	PegawaiUncheckedCreateInput,
+	PegawaiUncheckedUpdateInput,
 } from "@backend/generated/prisma/models.ts";
 import { CRUD } from "./__basicCRUD.ts";
 
@@ -11,4 +15,17 @@ export abstract class PegawaiService extends CRUD<
 	Pegawai,
 	PegawaiDelegate,
 	PegawaiInclude
->(Prisma.pegawai, { user: true, departemen: true, programStudi: true }) {}
+>(Prisma.pegawai, { user: true, departemen: true, programStudi: true }) {
+	public static create(data: PegawaiCreateInput | PegawaiUncheckedCreateInput) {
+		return Prisma.pegawai.create({
+			data: data as any,
+		});
+	}
+
+	public static update(id: string, data: PegawaiUpdateInput | PegawaiUncheckedUpdateInput) {
+		return Prisma.pegawai.update({
+			where: { id: id},
+			data: data as any,
+		});
+	}
+}
